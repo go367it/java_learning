4ec27a2af8dca014360b68ce34acba0326def799
go367it <satyamkumar3001@gmail.com>
2021-04-15 20:44:16 +0530

    implemented NCR code without functions

--- functions/Functions_in_java/src/Ncr.java
+++ functions/Functions_in_java/src/Ncr.java
@@ -3,6 +3,33 @@ import java.util.Scanner;
 public class Ncr {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("Hello world");
+        
+        int n = s.nextInt();
+        int r = s.nextInt();
+
+        int factN = 1;
+
+        // loop for doing n factorial
+        for(int i = 1; i<=n;i++){
+            factN = factN * i;
+        }
+
+        // loop for doing r factorial
+        int factR = 1;
+        for(int i = 1;i<=r;i++){
+            factR = factR * i;
+        }
+
+        //loop for doing the n-r factorial
+        int factN_R = n-r;
+        int N_r = 1;
+        for(int i =1;i<=factN_R;i++){
+            N_r = N_r * i;
+        }
+
+        int result = factN / (factR*N_r);
+
+        System.out.println("Result is: "+ result);
+
     }
 }
