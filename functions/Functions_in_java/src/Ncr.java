ba7a2f0e0dea3d64cd547aa76ec4a0a438bcf80a
go367it <satyamkumar3001@gmail.com>
2021-04-15 20:26:21 +0530

    started functions in java

---
+++ functions/Functions_in_java/src/Ncr.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Ncr {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
+}
